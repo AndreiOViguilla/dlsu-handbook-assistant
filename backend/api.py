32ce7c23a9aae55572a76e57b211f05bd06790ef
Andrei Dominic Viguilla <140049239+AndreiOViguilla@users.noreply.github.com>
2026-05-16 14:39:26 +0800

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -31,7 +31,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://your-app.vercel.app",   # replace with your Vercel URL
+        "https://dlsu-handbook-assistant.vercel.app",   # replace with your Vercel URL
     ],
     allow_methods=["POST", "DELETE", "GET"],
     allow_headers=["*"],
